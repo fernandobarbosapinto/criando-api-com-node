835381bec2b4372ad48b48e97035b5a405bc18d9
fernando barbosa <fpinto@igcorp.com.br>
2017-10-17 15:43:08 -0200

    Trabalhando com verbos GET, POST, PUT e DELETE

--- apitest/routes/index.js
+++ apitest/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET all customers */
+/* GET all customer */
 router.get('/customers', function(req, res, next){
 	var db = require('../db');
 	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
@@ -16,12 +16,58 @@ router.get('/customers', function(req, res, next){
 	});
 });
 
-/* GET one customers */
+/* GET one customer */
 router.get('/customers/:id', function(req, res, next){
 	var db = require('../db');
 	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
 	Customer.find({ _id: req.params.id }).lean().exec(function(e, docs){
 		res.json(docs);
+		res.end();
+	});
+});
+
+/* POST one customer */
+router.post('/customers', function(req, res, next){
+	var db = require('../db');
+	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+	var newCustomer = new Customer({ name: req.body.name, email: req.body.email });
+	newCustomer.save(function(err){
+		if(err){
+			res.status(500).json({ error: err.message });
+			res.end();
+			return;
+		}
+		res.json(newCustomer);
+		res.end();
+	});
+});
+
+/* PUT one customer */
+router.put('/customers/:id', function(req, res, next){
+	var db = require('../db');
+	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+	Customer.findOneAndUpdate({ _id: req.params.id }, req.body, { upsert: true }, function(err, doc){
+		if(err){
+			res.status(500).json({ error: err.message });
+			res.end();
+			return;
+		}
+		res.json(req.body);
+		res.end();
+	});
+});
+
+/* DELETE one customer */
+router.delete('/customers/:id', function(req, res, next){
+	var db = require('../db');
+	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+	Customer.find({ _id: req.params.id }).remove(function(err){
+		if(err){
+			res.status(500).json({ error: err.message });
+			res.end();
+			return;
+		}
+		res.json({ success: true });
 		res.end();
 	});
 });

5b10c8683f3592d4217e7fd798360299a7371ef1
fernando barbosa <fpinto@igcorp.com.br>
2017-10-17 12:07:44 -0200

    Configurando o ambiente e o banco de dados continuação e fazendo o primeiro GET

--- apitest/routes/index.js
+++ apitest/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET all customers */
+router.get('/customers', function(req, res, next){
+	var db = require('../db');
+	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+	Customer.find({}).lean().exec(function(e,docs){
+		res.json(docs);
+		res.end();
+	});
+});
+
+/* GET one customers */
+router.get('/customers/:id', function(req, res, next){
+	var db = require('../db');
+	var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+	Customer.find({ _id: req.params.id }).lean().exec(function(e, docs){
+		res.json(docs);
+		res.end();
+	});
+});
+
 module.exports = router;
